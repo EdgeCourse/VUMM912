3b091369076e95d33b05f1f3e96b9b10a5672b12
EdgeCourse <106573186+EdgeCourse@users.noreply.github.com>
2022-09-15 04:25:18 -0500

    Add files via upload

---
+++ customgenericdemo.go
@@ -0,0 +1,27 @@
+//generic for a general numeric
+
+//before generics were introduced in Go 1.18, people used work-arounds:
+//https://yourbasic.org/golang/generics/
+
+package main
+
+import (
+	"fmt"
+)
+
+//add name of generic
+//https://yourbasic.org/golang/interfaces-explained/
+type  interface {
+	//add rules
+	//~int //anything derived from int
+}
+
+//add generic type here
+func Add {
+	return a + b
+}
+
+func main() {
+	fmt.Println("4 + 3 =", Add(4, 3))
+	fmt.Println("4.1 + 3.2 =", Add(4.1, 3.2))
+}
